10d929618c07d496c766dfb69e74fbc9d742bf51
Oktavianus Bima Jadiva <93313454+Bimajadivaaa@users.noreply.github.com>
2023-11-22 02:43:45 +0700

    fix css at Request Funding page

--- app/src/pages/RequestFunding.jsx
+++ app/src/pages/RequestFunding.jsx
@@ -22,32 +22,31 @@ const RequestFunding = () => {
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(form)
-
+    console.log(form);
   };
 
   return (
-    <div className="bg-gradient-to-b from-black to-slate-900 flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
+    <div className="bg-gradient-to-b from-black to-slate-900 flex flex-col rounded-[10px] sm:p-10 p-4 md:items-center md:justify-center">
       {isLoading && "Loading...."}
-      <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]">
-        <h1 className="font-['Poppins'] font-bold text-white sm:text-[25px] text-[18px] leading-[38px]">
+      <div className="flex justify-center items-center p-[16px] sm:min-w-[150px] bg-[#4f4ffd] rounded-[10px]">
+        <h1 className="font-['Poppins'] item-start font-semibold text-white sm:text-[25px] text-[21px] leading-[38px]">
           Start Request Funding
         </h1>
       </div>
       <form
         onSubmit={handleSubmit}
-        className="w-full mt-[65px] flex flex-col gap-[30px]"
+        className="w-full md:w-[950px] mt-[65px] flex flex-col gap-[30px]"
       >
         <div className="flex flex-wrap gap-[40px]">
           <FormField
-            labelName="Masukan Nama Anda"
+            labelName="Masukan Nama Anda :"
             placeholder="Masukan nama anda"
             inputType="text"
             value={form.name}
             handleChange={(event) => handleFormFieldChange("name", event)}
           />
           <FormField
-            labelName="Masukan Judul Donasi Anda"
+            labelName="Masukan Judul Donasi Anda :"
             placeholder="Masukan judul anda"
             inputType="text"
             value={form.title}
@@ -55,7 +54,7 @@ const RequestFunding = () => {
           />
         </div>
         <FormField
-          labelName="Ceritakan Tentang Donasi Anda"
+          labelName="Ceritakan Tentang Donasi Anda :"
           placeholder="Masukan cerita anda"
           isTextArea
           value={form.description}
@@ -63,14 +62,14 @@ const RequestFunding = () => {
         />
         <div className="flex flex-wrap gap-[40px]">
           <FormField
-            labelName="Target Donasi"
+            labelName="Target Donasi :"
             placeholder="BSC 0.1"
             inputType="text"
             value={form.target}
             handleChange={(event) => handleFormFieldChange("target", event)}
           />
           <FormField
-            labelName="Masukan Tanggal Berakhir Donasi Anda"
+            labelName="Tanggal Berakhir :"
             placeholder="Masukan tanggal berakhir donasi anda"
             inputType="date"
             value={form.deadline}
@@ -78,16 +77,16 @@ const RequestFunding = () => {
           />
         </div>
         <FormField
-          labelName="Masukan Url Image anda"
+          labelName="Masukan Url Gambar Anda :"
           placeholder="Masukan link gambar"
           inputType="url"
           value={form.image}
           handleChange={(event) => handleFormFieldChange("image", event)}
         />
-        <div className="flex justify-center items-center mt-[10px]">
+        <div className="flex justify-center items-center mb-4">
           <CustomButton
             btnType="submit"
-            title="Submit new campaign"
+            title="Submit Campaign"
             styles="bg-[#1dc071]"
           />
         </div>
