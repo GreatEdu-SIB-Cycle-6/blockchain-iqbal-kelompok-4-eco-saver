d274143a51c55d5a2a18879e110ecbae8e13b9cc
Harun Al Rasyid <harundarat@gmail.com>
2023-11-28 22:01:01 +0700

    improve code for data retrieval

--- contract/contracts/CrowdFunding.sol
+++ contract/contracts/CrowdFunding.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
- 
+
 import "@openzeppelin/contracts/access/Ownable.sol";
- 
+
 interface IReward {
     function recordDonation(address donator, uint256 amount) external;
 }
- 
+
 interface IAdmin {
     function collectFee(uint256 _amount) external;
- 
+
     function isAdminExist(address _admin) external view returns (bool);
 }
- 
+
 contract CrowdFunding is Ownable {
     struct Campaign {
         address owner;
@@ -28,26 +28,24 @@ contract CrowdFunding is Ownable {
         uint256[] donations;
         bool isReleased;
     }
- 
+
     IReward public reward;
     IAdmin public admin;
     address adminContract;
     Campaign[] private requestList;
     Campaign[] private campaigns;
-    // mapping(uint256 => Campaign) private campaigns;
     mapping(address => bool) private hasRequest;
-    // uint256 private numberOfCampaigns = 0;
     uint256 public fundLocked;
- 
+
     constructor() Ownable(msg.sender) {}
- 
+
     receive() external payable {}
- 
+
     modifier onlyAdmin() {
         require(admin.isAdminExist(msg.sender), "You are not admin");
         _;
     }
- 
+
     event CampaignRequested(
         address indexed _owner,
         address indexed _requester,
@@ -78,7 +76,7 @@ contract CrowdFunding is Ownable {
         uint256 _fee,
         address _verifier
     );
- 
+
     // Request Campaign
     function requestCampaign(
         address _owner,
@@ -89,7 +87,7 @@ contract CrowdFunding is Ownable {
         string memory _image
     ) public returns (uint256) {
         address _requester = msg.sender;
- 
+
         require(
             hasRequest[_requester] == false,
             "You have already made a campaign request. Please wait until your request is processed first."
@@ -98,7 +96,7 @@ contract CrowdFunding is Ownable {
             _deadline > block.timestamp,
             "The deadline should be a date in the future."
         );
- 
+
         requestList.push(Campaign({
                 owner: _owner,
                 requester: _requester,
@@ -113,9 +111,9 @@ contract CrowdFunding is Ownable {
                 donations: new uint256[](0),
                 isReleased: false
             }));
- 
+
         hasRequest[_requester] = true;
- 
+
         emit CampaignRequested(
             _owner,
             _requester,
@@ -127,7 +125,7 @@ contract CrowdFunding is Ownable {
         );
         return requestList.length - 1;
     }
- 
+
     function _createCampaign(
         address _owner,
         address _requester,
@@ -141,7 +139,7 @@ contract CrowdFunding is Ownable {
             _deadline > block.timestamp,
             "The deadline should be a date in the future."
         );
- 
+
         campaigns.push(Campaign({
                 owner: _owner,
                 requester: _requester,
@@ -156,35 +154,22 @@ contract CrowdFunding is Ownable {
                 donations: new uint256[](0),
                 isReleased: false
         }));
- 
-        // Campaign storage campaign = campaigns[numberOfCampaigns];
- 
-        // campaign.owner = _owner;
-        // campaign.requester = _requester;
-        // campaign.title = _title;
-        // campaign.description = _description;
-        // campaign.target = _target;
-        // campaign.deadline = _deadline;
-        // campaign.amountCollected = 0;
-        // campaign.image = _image;
- 
-        // numberOfCampaigns++;
- 
+
         return campaigns.length - 1;
     }
- 
+
     // Delete request by id after it processed (approved or rejected)
     function _deleteRequest(uint256 _id,address _requester) private {
         delete hasRequest[_requester];
         requestList[_id] = requestList[(requestList.length - 1)];
         requestList.pop();
     }
- 
+
     // Approve campaign from request list then create the campaign
     function approveRequest(uint256 _id) external onlyAdmin {
         require(_id < requestList.length, "Campaign not exist");
         Campaign memory request = requestList[_id];
- 
+
         _createCampaign(
             request.owner,
             request.requester,
@@ -194,9 +179,9 @@ contract CrowdFunding is Ownable {
             request.deadline,
             request.image
         );
- 
+
         _deleteRequest(_id, request.requester);
- 
+
         emit CampaignAction(
             request.owner,
             request.requester,
@@ -208,13 +193,13 @@ contract CrowdFunding is Ownable {
             msg.sender
         );
     }
- 
+
     // Reject selected request and delete from requestList
     function rejectRequest(uint256 _id) external onlyAdmin {
         Campaign memory request = requestList[_id];
- 
+        
         _deleteRequest(_id, request.requester);
- 
+
         emit CampaignAction(
             request.owner,
             request.requester,
@@ -226,106 +211,93 @@ contract CrowdFunding is Ownable {
             msg.sender
         );
     }
- 
+
     // Donate funds then record amount into Reward contract
     function donateToCampaign(uint256 _id) external payable {
         address _donator = msg.sender;
         uint256 _amount = msg.value;
- 
+
         Campaign storage campaign = campaigns[_id];
- 
+
+        require(campaign.isReleased == false, "You cannot donate to a campaign that has already ended");
+
         campaign.donators.push(_donator);
         campaign.donations.push(_amount);
- 
+
         (bool sent, ) = payable(address(this)).call{value: _amount}("");
- 
+
         require(sent, "Send Ether failed");
- 
+
         fundLocked += _amount; // mencatat donasi yang terkumpul
         campaign.amountCollected += _amount;
         campaign.storedAmount += _amount;
         reward.recordDonation(_donator, _amount);
- 
+
         emit CampaignDonated(_donator, _amount, _id);
     }
- 
+
     // Release funds to the campaign owner
     function releaseFunds(uint256 _id) external payable onlyAdmin {
         address _campaignOwner = campaigns[_id].owner;
         uint256 _storedAmount = campaigns[_id].storedAmount;
- 
+
         require(_campaignOwner != address(0), "Can not send to address zero");
         require(_storedAmount != 0, "No funds can be released");
- 
+
         uint256 _amountAfterFee = (_storedAmount * 95) / 100; // platform fee = 5%
         uint256 _fee = _storedAmount - _amountAfterFee;
- 
+
         (bool success, ) = payable(adminContract).call{value: _fee}(""); // send fee into Admin contract
         require(success, "Can not collect fee");
         admin.collectFee(_fee);
- 
+
         (bool sent, ) = payable(_campaignOwner).call{value: _amountAfterFee}(
             ""
         );
- 
+
         require(sent, "Release of funds failed");
- 
+
         fundLocked -= _storedAmount;
         campaigns[_id].storedAmount = 0;
         campaigns[_id].isReleased = true;
- 
+
         emit FundReleased(_campaignOwner, _amountAfterFee, _fee, msg.sender);
     }
- 
+
     // Admin instance must be set first with Admin Contract address
     function setAdmin(address _admin) external onlyOwner {
         adminContract = _admin;
         admin = IAdmin(_admin);
     }
- 
+
     // Reward must be set first with Reward Contract address
     function setReward(address _reward) external onlyOwner {
         reward = IReward(_reward);
     }
- 
+
     function getDonators(
         uint256 _id
     ) external view returns (address[] memory, uint256[] memory) {
         return (campaigns[_id].donators, campaigns[_id].donations);
     }
- 
+
     // Get all campaigns
     function getCampaigns() external view returns (Campaign[] memory) {
- 
         return campaigns;
- 
-        // Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
- 
-        // for (uint i = 0; i < numberOfCampaigns; i++) {
-        //     Campaign storage item = campaigns[i];
- 
-        //     if (item.isReleased == false) {
-        //         allCampaigns[i] = item;
-        //     }
-        // }
- 
-        // return allCampaigns;
     }
- 
+
     // Get campaign by id
     function getCampaign(uint256 _id) external view returns (Campaign memory) {
         return campaigns[_id];
     }
- 
+
     // Get all pending request list
-    function getRequestList() external view returns (Campaign[] memory) {
- 
+    function getRequestList() external view onlyAdmin returns (Campaign[] memory) {
         return requestList;
- 
     }
- 
+
     // get request data by Id
-    function getRequest(uint256 _id) external view returns (Campaign memory) {
+    function getRequest(uint256 _id) external view onlyAdmin returns (Campaign memory) {
         return requestList[_id];
     }
 }
--- contract/contracts/Reward.sol
+++ contract/contracts/Reward.sol
@@ -18,35 +18,39 @@ contract Reward is Ownable{
         string image; // If the reward is an NFT, this field must be filled with image URI of the NFT
         bool isNft;
     }
+    struct Claim {
+        address claimant;
+        string shipAddress;
+        Item item;
+        uint256 claimDate;
+    }
 
     IEcoSaverNFT public ecoSaverNFT;
     address public crowdFundingAddr;
-    mapping (uint256 => Item) private rewardList;
-    uint256 public numberOfReward;
+    Item[] private rewardList;
     mapping (address => uint256) private donatorData;
-    // mapping (address => Item[]) public claimHistory;
+    Claim[] private shippingHistory;
 
     constructor() Ownable(msg.sender) {}
 
     event ItemAdded(string _name, string _description, Rarity _rarity, uint256 _minAmount, uint256 _remainingItem, string _image, bool _isNft);
-    event RewardClaimed(address _recipient, uint256 _rewardId);
+    event RewardClaimed(address _recipient, uint256 _rewardId, uint256 _claimDate);
 
     // Menambahkan item untuk reward
     function addItem(string calldata _name, string calldata _description, Rarity _rarity, uint256 _minAmount, uint256 _remainingItem, string calldata _image, bool _isNft) public onlyOwner returns(uint256) {
-        Item storage item = rewardList[numberOfReward];
 
-        item.name = _name;
-        item.description = _description;
-        item.rarity = _rarity;
-        item.minAmount = _minAmount;
-        item.remainingItem = _remainingItem;
-        item.image = _image;
-        item.isNft = _isNft;
-
-        numberOfReward++;
+        rewardList.push(Item({
+            name: _name,
+            description: _description,
+            rarity: _rarity,
+            minAmount: _minAmount,
+            remainingItem: _remainingItem,
+            image: _image,
+            isNft: _isNft
+        }));
 
         emit ItemAdded(_name, _description, _rarity, _minAmount, _remainingItem, _image, _isNft);
-        return numberOfReward - 1;
+        return rewardList.length - 1;
     }
 
     // Catat jumlah donasi tiap user
@@ -55,24 +59,39 @@ contract Reward is Ownable{
         donatorData[donator] += amount;
     }
 
-    function claimReward(uint256 _id) external {
-        uint256 _minAmount = rewardList[_id].minAmount;
+    // Claim a reward then record into claimHistory
+    function claimReward(uint256 _id, string memory _shipAddress) external {
+        Item storage reward = rewardList[_id];
+        require(reward.remainingItem > 0, "Selected item is out of stock");
+        uint256 _minAmount = reward.minAmount;
         address _donator = msg.sender;
 
         require(donatorData[_donator] >= _minAmount , "The amount of your donation is insufficient.");
-        bool isNft = rewardList[_id].isNft;
+        bool isNft = reward.isNft;
         
         if (isNft) {
-            string memory _imageURI = rewardList[_id].image;
+            string memory _imageURI = reward.image;
             ecoSaverNFT.mint(msg.sender, _imageURI);
             donatorData[_donator] -= _minAmount;
-            rewardList[_id].remainingItem -= 1;
+            reward.remainingItem -= 1;
         } else {
             donatorData[_donator] -= _minAmount;
-            rewardList[_id].remainingItem -= 1;
+            reward.remainingItem -= 1;
+            shippingHistory.push(Claim({
+                claimant: _donator,
+                shipAddress: _shipAddress,
+                item: reward,
+                claimDate: block.timestamp
+            }));
         }
         
-        emit RewardClaimed(_donator, _id);
+        emit RewardClaimed(_donator, _id, block.timestamp);
+    }
+
+    // Delete reward by id
+    function deleteReward(uint256 _id) external onlyOwner {
+        rewardList[_id] = rewardList[(rewardList.length - 1)];
+        rewardList.pop();
     }
 
     // ecoSaverNFT must be set first with EcoSaverNFT contract address
@@ -85,8 +104,19 @@ contract Reward is Ownable{
         crowdFundingAddr = _crowdfunding;
     }
 
+    // get all reward item
+    function getRewardList() external view returns (Item[] memory) {
+        return rewardList;
+    }
+
+    // get reward data by id
     function getReward(uint256 _id) external view returns (Item memory) {
         return rewardList[_id];
+    }
+
+    // get claim history
+    function getShippingHistory() external view returns (Claim[] memory) {
+        return shippingHistory;
     }
 
     // get donator data by address
